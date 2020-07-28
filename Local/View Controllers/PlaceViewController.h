e250bb9adfc9c471482c6dba66313fd7ddb1216e
Caroline Reiser <carolinereiser@fb.com>
2020-07-28 10:55:45 -0700

    created place view controller

---
+++ Local/View Controllers/PlaceViewController.h
@@ -0,0 +1,19 @@
+//
+//  PlaceViewController.h
+//  Local
+//
+//  Created by Caroline Reiser on 7/28/20.
+//  Copyright © 2020 Caroline Reiser. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface PlaceViewController : UIViewController
+
+@property (weak, nonatomic) IBOutlet UILabel *placeName;
+
+@end
+
+NS_ASSUME_NONNULL_END
