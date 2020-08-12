e4d801233f4af5c3e24a5ec70d997bebdc860a9b
Caroline Reiser <carolinereiser@fb.com>
2020-08-12 13:07:40 -0700

    more UI updates

--- Local/View Controllers/SpotsItineraryViewController.m
+++ Local/View Controllers/SpotsItineraryViewController.m
@@ -6,13 +6,14 @@
 //  Copyright © 2020 Caroline Reiser. All rights reserved.
 //
 
+#import <DZNEmptyDataSet/UIScrollView+EmptyDataSet.h>
 #import "Spot.h"
 #import "SpotsItineraryViewController.h"
 #import "TimelineCell.h"
 
 @import Parse;
 
-@interface SpotsItineraryViewController () <UITableViewDelegate, UITableViewDataSource>
+@interface SpotsItineraryViewController () <UITableViewDelegate, UITableViewDataSource, DZNEmptyDataSetSource, DZNEmptyDataSetDelegate>
 
 @property (nonatomic, strong) NSArray<Spot *> *spots;
 @property (weak, nonatomic) IBOutlet UITableView *tableView;
@@ -26,6 +27,10 @@
     // Do any additional setup after loading the view.
     self.tableView.delegate = self;
     self.tableView.dataSource = self;
+    
+    self.tableView.emptyDataSetSource = self;
+    self.tableView.emptyDataSetDelegate = self;
+    [self.tableView setSeparatorStyle:UITableViewCellSeparatorStyleNone];
     
     if(self.adminArea2) {
         self.titleLabel.text = [NSString stringWithFormat:@"Spots in %@", self.adminArea2];
@@ -78,6 +83,51 @@
 
 - (NSInteger)tableView:(nonnull UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
     return self.spots.count;
+}
+
+- (UIImage *)resizeImage:(UIImage *)image withSize:(CGSize)size {
+    UIImageView *resizeImageView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, size.width, size.height)];
+    
+    resizeImageView.contentMode = UIViewContentModeScaleAspectFill;
+    resizeImageView.image = image;
+    
+    UIGraphicsBeginImageContext(size);
+    [resizeImageView.layer renderInContext:UIGraphicsGetCurrentContext()];
+    UIImage *newImage = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    
+    return newImage;
+}
+
+- (UIImage *)imageForEmptyDataSet:(UIScrollView *)scrollView {
+    return [self resizeImage:[UIImage imageNamed:@"feed-icon"] withSize:(CGSizeMake(100,100))];
+}
+
+- (NSAttributedString *)titleForEmptyDataSet:(UIScrollView *)scrollView
+{
+    NSString *text = @"No Spots!";
+    
+    NSDictionary *attributes = @{NSFontAttributeName: [UIFont boldSystemFontOfSize:18.0f],
+                                 NSForegroundColorAttributeName: [UIColor systemGray5Color]};
+    
+    return [[NSAttributedString alloc] initWithString:text attributes:attributes];
+}
+
+- (NSAttributedString *)descriptionForEmptyDataSet:(UIScrollView *)scrollView {
+    NSString* text = @"";
+    if(self.adminArea2) {
+        text = [NSString stringWithFormat:@"Be the first to post a Spot from %@", self.adminArea2];
+    }
+    else if(self.adminArea) {
+        text = [NSString stringWithFormat:@"Be the first to post a Spot from %@", self.adminArea];
+    }
+    else {
+        text = [NSString stringWithFormat:@"Be the first to post a Spot from %@", self.country];
+    }
+    NSDictionary *attributes = @{NSFontAttributeName: [UIFont boldSystemFontOfSize:15.0f],
+                                 NSForegroundColorAttributeName: [UIColor systemGray6Color]};
+    
+    return [[NSAttributedString alloc] initWithString:text attributes:attributes];
 }
 /*
 #pragma mark - Navigation
